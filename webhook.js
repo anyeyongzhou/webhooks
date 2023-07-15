d88718d628a654494ecc55896f4d3561589c2501
anyeyongzhou <1354785752@qq.com>
2023-07-16 03:09:20 +0800

    init

--- webhook.js
+++ webhook.js
@@ -34,11 +34,13 @@ let server = http.createServer(function (req, res) {
         child.stdout.on("end", function () {
           let logs = Buffer.concat(buffers).toString();
           sendMail(`
-            部署日期: ${new Date()}
-            部署人: ${payload.pusher.name}
-            部署邮箱: ${payload.pusher.email}
-            提交信息: ${payload.head_commit && payload.head_commit["message"]}
-            布署日志: ${logs.replace("\r\n", "<br/>")}
+            <h1>部署日期: ${new Date()}</h1>
+            <h2>部署人: ${payload.pusher.name}</h2>
+            <h2>部署邮箱: ${payload.pusher.email}</h2>
+            <h2>提交信息: ${
+              payload.head_commit && payload.head_commit["message"]
+            }</h2>
+            <h2>布署日志: ${logs.replace("\r\n", "<br/>")}</h2>
         `);
         });
       }
