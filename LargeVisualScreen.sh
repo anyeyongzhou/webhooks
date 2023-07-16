e56483c8ab45c7b22fbb705e8644488208783594
anyeyongzhou <1354785752@qq.com>
2023-07-16 21:18:15 +0800

    add webhookjs

--- LargeVisualScreen.sh
+++ LargeVisualScreen.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 WORK_PATH='/usr/projects/LargeVisualScreen'
 cd $WORK_PATH
-echo "清理代码"
-git reset --hard origin/master
-git clean -f
 echo "拉取最新代码"
-git pull origin master
+git pull origin main
 echo "打包最新代码"
 npm run build
 echo "开始构建镜像"
--- cool-admin-midway.sh
+++ cool-admin-midway.sh
@@ -5,7 +5,7 @@ echo "先清理老代码"
 git reset --hard origin/master
 git clean -f
 echo "拉取最新代码"
-git pull origin master
+git pull origin main
 echo "开始构建镜像"
 docker build -t cool-admin-midway .
 echo "删除旧容器"
