abb8b64d9baf9208e6003311f89b601d7d2c2c33
anyeyongzhou <1354785752@qq.com>
2023-07-17 13:30:21 +0800

    add webhookjs

--- sendMail.js
+++ sendMail.js
@@ -5,8 +5,8 @@ const smtp = "FIECWWXLIRPRWUJH"; //在邮箱中获取的smtp码
 let transporter = nodemailer.createTransport({
   // host: 'smtp.ethereal.email',
   service: "163", // 使用了内置传输发送邮件，支持qq、163、
-  port: 465, // SMTP 端口
-  secureConnection: true, // 使用了 SSL
+  //port: 465, // SMTP 端口
+  //secureConnection: true, // 使用了 SSL
   auth: {
     user: user,
     pass: smtp,
@@ -15,11 +15,11 @@ let transporter = nodemailer.createTransport({
 
 function sendMail(message) {
   let mailOptions = {
-    from: "汪全玉" + user, // 发送者
+    from: user, // 发送者
     to: "1354785752@qq.com", // 接收者，多个接受者用,隔开
-    subject: "部署通知", // 邮件主题
+    subject: "汪全玉的部署通知", // 邮件主题
     html: message, // HTML 邮件内容
-    // text: 'Hello World!', // 纯文本邮件内容
+    //text: '汪全玉的邮件', // 纯文本邮件内容
   };
   // send mail with defined transport object
   transporter.sendMail(mailOptions, (error, info) => {
